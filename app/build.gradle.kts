6a359e3698621674d2b30a70f42b9a1e65a02285
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-05-09 01:28:54 +0300

    update Java version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    // Google Map Compose
     implementation(libs.android.google.map.compose)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
