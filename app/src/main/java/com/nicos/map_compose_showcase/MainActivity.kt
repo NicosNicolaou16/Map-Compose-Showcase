fb8f9ce3ee1adc152e9ba884dfb92d48c1ba693a
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-05-09 01:58:33 +0300

    connect the screen

--- app/src/main/java/com/nicos/map_compose_showcase/MainActivity.kt
+++ app/src/main/java/com/nicos/map_compose_showcase/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.nicos.map_compose_showcase.map_compose.GoogleMapComposeRoot
 import com.nicos.map_compose_showcase.ui.theme.MapComposeShowcaseTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,28 +23,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             MapComposeShowcaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    GoogleMapComposeRoot()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MapComposeShowcaseTheme {
-        Greeting("Android")
     }
 }
