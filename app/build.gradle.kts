f59fa0e6bf8ae73019b2c957fca143d75065be67
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-05-09 11:48:02 +0300

    enable the R8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,16 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
