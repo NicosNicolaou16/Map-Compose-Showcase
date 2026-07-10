e0676174b915d5dcbed8b02fd4f565c750477f9a
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-07-10 22:16:26 +0300

    update Target SDK to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
     defaultConfig {
         applicationId = "com.nicos.map_compose_showcase"
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
